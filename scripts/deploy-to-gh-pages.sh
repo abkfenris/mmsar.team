61bc7b856038ff0f84723ce35376692aa7fff518
Alex Kerney <abk@mac.com>
2016-07-11 10:13:56 -0400

    Trying http://www.steveklabnik.com/automatically_update_github_pages_with_travis_example/ instructions instead

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
-set -o errexit
+set -o errexit -o nounset
 
 if [ "$TRAVIS_BRANCH" != "master" ]
 then
-  echo "This commit was made agains the $TRAVIS_BRANCH and not the master. Not deploying to Github pages"
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master. Not deploying to Github pages"
   exit 0
 fi
 
@@ -12,8 +12,7 @@ rm -rf output
 mkdir output
 
 # config
-git config --global user.email "abk@mac.com"
-git config --global user.name "Alex Kerney"
+rev=$(git rev-parse --short HEAD)
 
 # build
 nikola clean
@@ -22,6 +21,20 @@ nikola build
 # deploy
 cd output
 git init
-git add .
-git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git config --global user.email "abk@mac.com"
+git config --global user.name "Alex Kerney"
+
+#git commit -m "Deploy to Github Pages"
+#git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+
+git remote add upstream "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+echo "mmsar.team" > CNAME
+
+touch .
+
+git add -A .
+git commit -m "rebuild Github Pages by Travis at ${rev}"
+git push -q upstream HEAD:gh-pages
